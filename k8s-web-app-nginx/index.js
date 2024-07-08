b9dd49590ba898b598925f7509e23d5dfc8ee29f
Tamojit Das <tamojitdas181007@gmail.com>
2024-07-08 19:54:19 +0530

    NGINX endpoint init

---
+++ k8s-web-app-nginx/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import fetch from "node-fetch";
+import os from "os";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    const message = `Hello from ${os.hostname()}`;
+    console.log(message);
+    res.send(message);
+});
+
+app.get("/nginx", async (req, res) => {
+    const url = 'http://nginx'; //connecting to nginx deployment endpoint by its static name, rather than its dynamic IP address
+    const response = await fetch(url);
+    const body = await response.text();
+    res.send(body);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
